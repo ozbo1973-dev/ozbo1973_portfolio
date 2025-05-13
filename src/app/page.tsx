be851fea3acec38818ad1fa2c07b8c0cb814d472
ozbo1973-dev <bbbove20.dev@gmail.com>
2025-05-12 20:17:01 -0600

    add about section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from "@/components/sections/hero";
+import About from "@/components/sections/about";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      {/* Other components */}
+      <About />
     </main>
   );
 }
